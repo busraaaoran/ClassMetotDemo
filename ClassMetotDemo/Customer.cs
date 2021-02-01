9fda122ae51392a049f1bb408f43b3267c037013
Büşra <busraoran043@gmail.com>
2021-02-01 17:19:44 +0300

    ClassMetotDemo

---
+++ ClassMetotDemo/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class Customer
+    {
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+
+        public string Surname { get; set; }
+
+        public int PhoneNumber { get; set; }
+
+    }
+}
